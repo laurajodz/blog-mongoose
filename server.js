a52925c8d02ddfc84509ef0f3444fcb32e77009e
Laura Jodz <laurajodz@gmail.com>
2017-12-17 11:50:17 -0800

    adding config and server files

---
+++ server.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {BlogPost} = require('./models');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.get('/blog-posts', (req, res) => {
+  BlogPost
+    .find()
+    .then(blog-posts => {
+      res.json({
+        blog-posts: blog-posts.map(
+          
+        )
+      })
+    })
+})
+
+let server;
+
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, {useMongoClient: true}, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
